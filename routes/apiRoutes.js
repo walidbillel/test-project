184d34852e83ad6ae96239b4af53ec24ae429730
willou <walid.boubguira@gmail.com>
2019-01-24 16:48:27 -0500

    check

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,58 @@
-var db = require('../models');
+var db = require("../models");
 
-module.exports = function (app) {
-
-
-    app.get("/api/users", function (req, res) {
-        db.User.findAll({}).then(function (dbUser) {
-            res.json(dbUser)
-        });
+module.exports = function(app) {
+  app.get("/api/users", function(req, res) {
+    db.User.findAll({}).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-    app.get("/api/users/:id", function (req, res) {
-        db.User.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        })
+  app.get("/api/users/:id", function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-    app.get("/api/users/:username/:password", function (req, res) {
-        // Find one User with the id in req.params.id and return them to the user with res.json
+  app.get("/api/users/:username/:password", function(req, res) {
+    // Find one User with the id in req.params.id and return them to the user with res.json
 
-        db.User.findOne({
-            where: {
-                Username: req.params.username,
-                password: req.params.password
-            }
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        })
+    db.User.findOne({
+      where: {
+        Username: req.params.username,
+        password: req.params.password
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-    app.get("/api/users/password/:realname/:email", function (req, res) {
-        // Find one User with the id in req.params.id and return them to the user with res.json
+  app.get("/api/users/password/:realname/:email", function(req, res) {
+    // Find one User with the id in req.params.id and return them to the user with res.json
 
-        db.User.findOne({
-            where: {
-                realName: req.params.realname,
-                email: req.params.email
-            }
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        })
+    db.User.findOne({
+      where: {
+        realName: req.params.realname,
+        email: req.params.email
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-
-
-    app.post("/api/users", function (req, res) {
-        // Create an User with the data available to us in req.body
-        console.log(req.body);
-        db.User.create(req.body).then(function (dbUser) {
-            res.json(dbUser);
-        }).catch(function (err) {
-            console.log(err)
-            res.json(err)
-        });
-    });
-}
+  app.post("/api/users", function(req, res) {
+    // Create an User with the data available to us in req.body
+    console.log(req.body);
+    db.User.create(req.body)
+      .then(function(dbUser) {
+        res.json(dbUser);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json(err);
+      });
+  });
+};
