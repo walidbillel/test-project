ef23c4f749246ea2f1c7aabcd380ad0c79dc5850
willou <walid.boubguira@gmail.com>
2020-02-24 06:12:14 -0500

    changes

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
       // Giving the User model a name of type STRING
-      realName: DataTypes.STRING,
+      realName: {type: DataTypes.STRING},
       Username: { type: DataTypes.STRING, unique: true },
       image: {type: DataTypes.STRING},
       email: { type: DataTypes.STRING, unique: true },
